e729899953d5fcab782a753bab06ef72edfcf0d0
xItsJacob <jacobpears78@gmail.com>
2024-02-09 12:03:05 -0600

    fixed filtered_data bug

--- components/legislative.py
+++ components/legislative.py
@@ -13,16 +13,19 @@ class LegislativeTracker():
         self.time_format = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime())
 
     async def current_congress(self):
-        congress_num = requests.get(f'https://api.congress.gov/v3/congress?limit=1&api_key={os.getenv("CONGRESS_API_KEY")}') 
+        congress_num = requests.get(
+            f'https://api.congress.gov/v3/congress?limit=1&api_key={os.getenv("CONGRESS_API_KEY")}')
         print(congress_num.status_code)
-        congress_json = congress_num.json() if congress_num and congress_num.status_code == 200 else None
+        congress_json = congress_num.json(
+        ) if congress_num and congress_num.status_code == 200 else None
 
         unfilitered_name = congress_json['congresses'][0]['name']
         congress_name = ''.join(c for c in unfilitered_name if c.isdigit())
-        
+
         return congress_name
 
-    async def latest_update(self): # Data will be contained in a matrix, and all the data matrixis will be contained in its own matrix
+    # Data will be contained in a matrix, and all the data matrixis will be contained in its own matrix
+    async def latest_update(self):
 
         current_congress = await self.current_congress()
 
@@ -30,13 +33,14 @@ class LegislativeTracker():
         fromTime = '2024-01-07T00%3A00%3A00Z'
         toDate = '2024-02-07T00%3A00%3A00Z'
 
-        updated_bills = requests.get(f'https://api.congress.gov/v3/bill/{current_congress}?fromDateTime={fromTime}&toDateTime={toDate}&sort=updateDate+desc&api_key={os.getenv("CONGRESS_API_KEY")}')
+        updated_bills = requests.get(
+            f'https://api.congress.gov/v3/bill/{current_congress}?fromDateTime={fromTime}&toDateTime={toDate}&sort=updateDate+desc&api_key={os.getenv("CONGRESS_API_KEY")}')
         bills_response = updated_bills.json()
 
         if 'bills' in bills_response:
             bills_data = bills_response['bills']
-            if len(bills_data) > 0:  
-                filtered_data =   []
+            if len(bills_data) > 0:
+                filtered_data = []
                 for bill in bills_data:
                     type = bill['type']
                     number = bill['number']
@@ -44,7 +48,7 @@ class LegislativeTracker():
                     updateDate = bill['updateDateIncludingText']
                     text = bill['latestAction']['text']
 
-                    filtered_data.extend([
+                    filtered_data.append([
                         ['type', type],
                         ['number', number],
                         ['title', title],
@@ -57,6 +61,7 @@ class LegislativeTracker():
                 print(f"No bills: {self.time_format}")
         else:
             print('No bill key found. Potential error.')
+
 
 trackers = LegislativeTracker()
 
